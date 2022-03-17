a1dc5f465c23d696c0eda883b3d73543da6e1e61
Multra <Multra@gmail.com>
2022-03-17 11:56:47 -0500

    Delete bucket.py

--- bucket.py
+++
@@ -1,9 +0,0 @@
-import pulumi
-import pulumi_aws as aws
-
-bucket = aws.s3.Bucket("bucketpulumitest",
-    acl="private",
-    tags={
-        "Environment": "Dev",
-        "Name": "My bucket",
-    })
